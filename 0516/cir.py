56e50dbc7b1015f6339e72fb66a46e264728b2bb
highlight1016 <56434534+highlight1016@users.noreply.github.com>
2020-05-16 13:13:53 +0900

    Add files via upload

---
+++ 0516/cir.py
@@ -0,0 +1,10 @@
+#원의 면적을 구하는 프로그램을 작성하시오.
+#원의 면적 =  pi * r * r
+import math
+
+radius = int(input("반지름 입력 : "))
+
+area = 3.14159 * radius * radius
+
+print("반지름이", radius,"인 원의 면적은", area, "입니다.")
+             
