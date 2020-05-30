b6c91ec961884e85748f7c2bab2cf446070b6201
highlight1016 <56434534+highlight1016@users.noreply.github.com>
2020-05-30 11:34:32 +0900

    Add files via upload

---
+++ prob101.py ~ prob130.py/prob114.py
@@ -0,0 +1,7 @@
+a = input("입력값: ")
+num = 20 + int(a)
+if num > 255:
+    print(255)
+else:
+    print(num)
+
---
+++ prob101.py ~ prob130.py/prob127.py
@@ -0,0 +1,5 @@
+a = input("주민등록번호: ")
+if a[0] == "1" or a[0] == "3":
+    print("남자")
+else:
+    print("여자")
