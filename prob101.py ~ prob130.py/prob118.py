1b37ca0b28a0376385dada8f4a52e0b2c73fd4b8
highlight1016 <56434534+highlight1016@users.noreply.github.com>
2020-05-30 11:23:35 +0900

    Delete prob118.py

--- prob101.py ~ prob130.py/prob118.py
+++
@@ -1,6 +0,0 @@
-warn_investment_list = ["Microsoft", "Google", "Naver", "Kakao", "SAMSUNG", "LG"]
-a = input("종목명: ")
-if a in warn_investment_list:
-    print("투자 경고 종목입니다.")
-else:
-    print("투자 경고 종목이 아닙니다.")
