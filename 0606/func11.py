ee5ae55578b055461881a18fbc4e0b3be0710b37
highlight1016 <56434534+highlight1016@users.noreply.github.com>
2020-06-13 01:02:56 +0900

    Add files via upload

---
+++ 0606/func11.py
@@ -0,0 +1,8 @@
+import math
+
+def circle_area(radius):
+    area = math.pi * radius * radius  #area = 3.14159 * radius ** 2
+    return area
+
+r = int(input("반지름 : "))
+print("반지름", r,"의 넓이 :", circle_area(r))
